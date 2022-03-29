3b51afd1da7c6da11515d0fb8a91b8b3256dd558
Kunal Khandelwal <78425795+It-is-KunalKhandelwal@users.noreply.github.com>
2022-03-29 22:09:57 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,48 +1,93 @@
-// // // Ensure that the browser supports the service worker API
-// // if (navigator.serviceWorker) {
-// //     // Start registration process on every page load
-// //     window.addEventListener('load', () => {
-// //         navigator.serviceWorker
-// //             // The register function takes as argument
-// //             // the file path to the worker's file
-// //             .register('/service-worker.js')
-// //             // Gives us registration object
-// //             .then(reg => console.log('Service Worker Registered'))
-// //             .catch(swErr => console.log(
-// //                     `Service Worker Installation Error: ${swErr}}`));
-// //         });
-// //     }
-    
-//     var cacheName = 'tasty-v1';
-//     var cacheAssets = ['/'];
-    
-//     // Call install Event
-//     self.addEventListener('install', e => {
-//         // Wait until promise is finished
-//         e.waitUntil(
-//             caches.open(cacheName)
-//             .then(cache => {
-//                 console.log(`Service Worker: Caching Files: ${cache}`);
-//                 cache.addAll(cacheAssets)
-//                     // When everything is set
-//                     .then(() => self.skipWaiting())
-//             })
-//         );
-//     })
+
 if('serviceWorker' in navigator){
     navigator.serviceWorker.register('./service-worker.js')
       .then((reg)=>console.log('registred successfully',reg))
       .catch((error)=>console.log('registration failed',error))
 
 }
-//install
+//installing
+const cacheName='tasty-v2'
+const cacheAssets = [
+    '/',
+    './index.html',
+    './assets/css/styles.css',
+    './assets/scss/styles.scss',
+    './assets/js/main.js',
+    
+    './assets/img/192.png',
+    './assets/img/about.jpg',
+    './assets/img/app1.png',
+    './assets/img/app2.png',
+    './assets/img/dish.svg',
+    './assets/img/fast-food-12.png',
+    './assets/img/home.png',
+    './assets/img/movil-app.png',
+    './assets/img/pizza.svg',
+    './assets/img/plate1.png',
+    './assets/img/plate2.png',
+    './assets/img/plate3.png',
+    './assets/img/truck.svg',
+    'https://unpkg.com/scrollreveal'
+   
+
+];
 self.addEventListener('install',evt=>{
-    console.log('service installed')
+    // console.log('service installed')
+    evt.waitUntil(
+        caches.open(cacheName).then(cache=>{
+            console.log('caching files')
+            cache.addAll(cacheAssets)
+            .then(() => self.skipWaiting())
+        })
+    );
+    
 })
+//activate service worker
 self.addEventListener('activate',evt=>{
-    console.log('activated successfully')
+    evt.waitUntil(
+        caches.keys().then(keys=>{
+            return Promise.all(keys
+                .filter(key => key!==cacheName)
+                .map(key => caches.delete(key))
+            )
+        })
+
+        
+    )
 })
 //fetch
 self.addEventListener('fetch',evt=>{
-    console.log('fetch event:',evt)
+    //console.log('fetch event:',evt)
+    evt.respondWith(
+        caches.match(evt.request).then(res=>{
+            return res || fetch(evt.request)
+        })
+    );
+
+
+
+
 })
+// self.addEventListener('push', function(e) {
+//     console.log('bhbh')
+//     var options = {
+//       body: 'This notification was generated from a push!',
+//       icon: './assets/img/fast-food-12.png',
+//       vibrate: [100, 50, 100],
+//       data: {
+//         dateOfArrival: Date.now(),
+//         primaryKey: '2'
+//       },
+//       actions: [
+//         {action: 'explore', title: 'Explore this new world',
+//           icon: './assets/img/about.jpg'},
+//         {action: 'close', title: 'Close',
+//           icon: './assets/img/home.png'},
+//       ]
+//     };
+//     e.waitUntil(
+//       self.registration.showNotification('Hello world!', options)
+//     );
+//   });
+
+
